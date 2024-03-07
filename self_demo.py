943f51febfeb7cbd350b11a32ba1943232be402c
Vishal Thorat <115708806+VishalThorat21@users.noreply.github.com>
2024-03-08 01:01:48 +0530

    Add files via upload
     add file

---
+++ self_demo.py
@@ -0,0 +1,10 @@
+
+
+
+
+value="vishal"
+input=str(input("Enter the password"))
+if  value=="vishal":
+    print("Welocme to  vishals worlds.")
+else:
+    print("you  are eligiable  to enters the vishal Worlds")
